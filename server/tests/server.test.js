b3ece4dd9b8e22cc6447c3d6a8d97849f9870e8f
Kaushalendra Kumar Singh <kksinghtomar.1996@gmail.com>
2018-07-23 19:47:58 +0530

    Added tests for GET /todos

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -5,9 +5,19 @@ const expect = require('expect');
 const {app} = require('./../server.js');
 const {Todo} = require('./../models/todo');
 
+const todos = [{
+  text: 'first test todo'
+},{
+  text: 'second test todo'
+}]//array of objects
+
 beforeEach((done)=>{//delete all todos database
-  Todo.remove({}).then(()=>done());
-})
+  Todo.remove({}).then(()=>{
+    return Todo.insertMany(todos);//the array of objects
+
+  }).then(()=>done());//expression format and not the function format
+});
+
 describe('POST /todos',()=>{
   it('should create a new todo',(done)=>{
     var text = 'Test todo text';
@@ -24,7 +34,7 @@ describe('POST /todos',()=>{
         return done(err)//done wraps the test cases
       }
 
-      Todo.find().then((todos)=>{
+      Todo.find({text}).then((todos)=>{//only text pertaininf here is returned , so todos.length ===1
         expect(todos.length).toBe(1);//fields in the document is same as the passed body
         expect(todos[0].text).toBe(text);//string matching of the text passed w.r.t. mongodb data
         done();
@@ -48,11 +58,23 @@ describe('POST /todos',()=>{
       }
 
       Todo.find().then((todos)=>{
-        expect(todos.length).toBe(0);//fields in the document is same as the passed body
+        expect(todos.length).toBe(2);//earlier 0//fields in the document is same as the passed body
         //expect(todos[0].text).toBe(text);//string matching of the text passed w.r.t. mongodb data
         done();
       }).catch((e)=>done(e))//if any error not mentioned is caught
     })//end ends
   })//it test case ends
+})
 
+describe('GET /todos',()=>{
+  it('should get all todos',(done)=>{
+    request(app)
+    .get ('/todos')
+  //  .send({})//unlike POST , not used here , no response to post or send
+    .expect(200)
+    .expect((res)=>{
+      expect(res.body.todos.length).toBe(2)//expect res//custom assertion , so using function
+  })
+  .end(done);//no need of functuoin like in POST as we are not doing anythibng asynchrously
+})
 })
